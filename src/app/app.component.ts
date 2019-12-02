4fa51db461df9b1825fc81fd0c8a253b27e33752
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2019-12-02 19:27:57 +0200

    added pptxgenJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FavChangedInfo } from './favorite.component';
+import * as PptxGenJS from 'pptxgenjs';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,16 @@ export class AppComponent {
   };
 
   viewMode = 'map';
+
+  generatePpt() {
+    const pptx = new PptxGenJS();
+    pptx.setBrowser(true);
+    const slide = pptx.addNewSlide();
+    slide.addText('BONJOUR - CIAO - GUTEN TAG - HELLO - HOLA - NAMASTE - OLÀ - ZDRAS-TVUY-TE - こんにちは - 你好',
+    { x: 0, y: 1, w: '100%', align: 'center', h: 2, color: '0088CC', fill: 'F1F1F1', fontSize: 24 });
+    pptx.save('Sample presentation');
+    console.log(slide);
+  }
 
   onFavoriteChange(newEvent: FavChangedInfo) {
     this.post.isFavorite = !this.post.isFavorite;
